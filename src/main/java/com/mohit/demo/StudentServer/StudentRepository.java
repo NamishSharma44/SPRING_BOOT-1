8d1b6808f903d29e51a579fc450eb52aecaab091
Mohit <hllomohit722@gmail.com>
2026-07-14 15:32:00 +0530

    StudentServer

---
+++ src/main/java/com/mohit/demo/StudentServer/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.mohit.demo.StudentServer;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+
+    public Student save(Student student) {
+        System.out.println("Student Information saved");
+        return student;
+    }
+}
