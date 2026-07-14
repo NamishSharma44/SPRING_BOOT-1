dba2b3c4c0373382fe5c21ca74004b0a98bcc011
Namish <namish907@gmail.com>
2026-07-14 11:07:20 +0530

    commit MySelf

---
+++ demo/src/main/java/com/namish/demo/Java/test.java
@@ -0,0 +1,4 @@
+package com.namish.demo.Java;
+
+public class test {
+}
---
+++ demo/src/main/java/com/namish/demo/Portfolio/Myportfolio.java
@@ -0,0 +1,33 @@
+package com.namish.demo.Portfolio;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Myportfolio {
+
+    @GetMapping("/myself")
+    public String myself(){
+        return """
+                <h1>Namish Sharma</h1>
+                <p>A good human being<p>
+                """;
+    }
+
+    @GetMapping("/skills")
+    public String skills(){
+        return """
+                <h1>SKILLs</h1>
+                <ul>
+                <li>github link: https://github.com/NamishSharma44</li>
+                </ul>
+                """;
+    }
+
+    @GetMapping("/edu")
+    public String edu(){
+        return "LASEC, Jhalawar(RAJ)";
+    }
+
+}
