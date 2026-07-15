d2d86716221c3b511284115be202ce83a5b0df0f
Namish <namish907@gmail.com>
2026-07-15 10:23:48 +0530

    post with student

--- demo/src/main/java/com/namish/demo/StudentServer/Student.java
+++ demo/src/main/java/com/namish/demo/StudentServer/Student.java
@@ -1,4 +1,41 @@
 package com.namish.demo.StudentServer;
 
-public class student {
+public class Student {
+
+    int id;
+    String name;
+    int age;
+    String department;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
--- demo/src/main/java/com/namish/demo/StudentServer/StudentRepository.java
+++ demo/src/main/java/com/namish/demo/StudentServer/StudentRepository.java
@@ -1,4 +1,12 @@
 package com.namish.demo.StudentServer;
 
-public class studentRepository {
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+
+    public Student save(Student student) {
+        System.out.println("Student Information saved");
+        return student;
+    }
 }
